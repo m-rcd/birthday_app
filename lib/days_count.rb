8b2d41c2a762fb124501d42c42a6cd56d3002efc
Marianne Rachid <marianne.rachid@gmail.com>
2018-09-20 13:45:09 +0100

    refactor

--- lib/days_count.rb
+++ lib/days_count.rb
@@ -1,14 +1,9 @@
-
 class Counter
   def count(day, month)
     birthday = month.capitalize! + ' ' + day
     bday = DateTime.parse(birthday)
     today = Date.today
-    if bday < today
-      next_birthday = bday + 365
-    else
-      next_birthday = bday
-    end
+    bday < today ? next_birthday = bday + 365 : next_birthday = bday
     days_until_birthday = (next_birthday - today).to_i
   end
 end
