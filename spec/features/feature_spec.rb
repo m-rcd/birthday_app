4dc6b3b92a97f0ecfeeb22559a0b9cf0d8fc68cc
Marianne Rachid <marianne.rachid@gmail.com>
2018-09-20 13:54:55 +0100

    fix test

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,8 +1,24 @@
 require 'spec_helper'
 require './birthday_app'
 
+RSpec.feature 'Testing homepage' do
+  scenario 'greeting' do
+    visit'/'
+    expect(page).to have_content('Hello there!')
+  end
 
-RSpec.feature "Entering information" do
+  scenario 'ask for name' do
+    visit '/'
+    expect(page).to have_content("What's your name?")
+  end
+
+  scenario 'ask for birthday' do
+    visit '/'
+    expect(page).to have_content("When's your birthday?")
+  end
+end
+
+RSpec.feature "Testing result" do
   scenario "today is the user's birthday" do
     allow(Date).to receive(:today).and_return('September 19')
     log_info
